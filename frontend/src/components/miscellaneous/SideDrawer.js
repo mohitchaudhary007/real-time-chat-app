12f3a04cb0451f31c03c8eb9e23ebc1bb2b3f4e4
Mohit Kumar <119070038+mohitchaudhary007@users.noreply.github.com>
2025-01-28 00:55:19 +0530

    SideDrawer.js

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -142,7 +142,7 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
+          Real-Time-Chat-App
         </Text>
         <div>
           <Menu>
